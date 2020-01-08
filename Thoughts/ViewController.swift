ee00c21ac51ad7f7e6cb759e361463ee5f8414cf
Jeremy Jung <jjchan921@gmail.com>
2020-01-07 21:50:55 -0600

    Initial Commit

---
+++ Thoughts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thoughts
+//
+//  Created by Jeremy Jung on 1/7/20.
+//  Copyright © 2020 Jeremy Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
